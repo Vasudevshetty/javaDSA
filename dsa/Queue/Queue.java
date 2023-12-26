e214c7db1ca64b7a1c39a1a5aa223ffb4552852f
Vasudevshetty <vasudeepu2815@gmail.com>
2023-12-26 19:59:34 +0530

    Added overrided toString() method

--- dsa/Queue/Queue.java
+++ dsa/Queue/Queue.java
@@ -4,34 +4,48 @@ import dsa.LinkedList.NodeSingle;
 import dsa.LinkedList.SingleLinkedList;
 import dsa.Queue.Exception.QueueEmptyException;
 
-public class Queue<E> {
+public class Queue<E> implements que<E> {
     SingleLinkedList<E> que;
-    
-    public Queue(){
+
+    public Queue() {
         que = new SingleLinkedList<>();
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return que.isEmpty();
     }
-    public void enqueue(E value){
+
+    @Override
+    public void enqueue(E value) {
         que.insertRear(value);
     }
-    public E dequeue(){
-        if(isEmpty()) throw new QueueEmptyException("Queue is empty, Dequeue failed.\n");
+
+    @Override
+    public E dequeue() throws QueueEmptyException{
+        if (isEmpty())
+            throw new QueueEmptyException("Queue is empty, Dequeue failed.\n");
         return que.deleteHead();
     }
-    public int length(){return que.getLength();}
-    public E peek(){
-        if(isEmpty()) throw new QueueEmptyException("Queue is empty, no element to peek.\n");
+
+    @Override
+    public int length() {
+        return que.getLength();
+    }
+
+    @Override
+    public E peek() throws QueueEmptyException{
+        if (isEmpty())
+            throw new QueueEmptyException("Queue is empty, no element to peek.\n");
         return que.getHead().getData();
     }
+
     @Override
-    public String toString(){
+    public String toString() {
+        if(isEmpty()) throw new QueueEmptyException("Queue is empty, printing failed.\n");
         StringBuilder queue = new StringBuilder();
         NodeSingle<E> temp = que.getHead();
         queue.append("Elements of the queue are, \n");
-        while(temp != null){
+        while (temp != null) {
             queue.append(temp.getData()).append(" ");
             temp = temp.getNext();
         }
