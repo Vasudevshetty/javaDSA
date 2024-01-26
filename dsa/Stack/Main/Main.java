26221340e7d1da5eec67cf8d2da305beb27b99f2
Vasudevshetty <vasudeepu2815@gmail.com>
2024-01-26 13:18:48 +0530

    added main testing thing

--- dsa/Stack/Main/Main.java
+++ dsa/Stack/Main/Main.java
@@ -1,10 +1,13 @@
 package dsa.stack.Main;
 
-import dsa.stack.PostfixConverter;
+import dsa.stack.NotationConvertor;
+import dsa.stack.NotationConvertor.NotationType;
 
 public class Main {
     public static void main(String[] args) {
-        String infix = "a+b+c*(d/e)"; 
-        System.out.println(PostfixConverter.postfix(infix));
+        String infix = "2*3-5"; 
+        System.out.println(NotationConvertor.postfix(infix) + " " + NotationConvertor.prefix(infix));
+        System.out.println(NotationConvertor.evaulate(NotationConvertor.prefix(infix), NotationType.prefix));
+        System.out.println(NotationConvertor.evaulate(NotationConvertor.postfix(infix), NotationType.postfix));
     }  
 }
