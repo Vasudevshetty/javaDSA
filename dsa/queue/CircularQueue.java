1e1a97caf638ef1ac45d588cc9e6d09100540e93
Vasudevshetty <vasudeepu2815@gmail.com>
2024-02-07 21:16:48 +0530

    Refactored code and added javaDoc comments.

--- dsa/queue/CircularQueue.java
+++ dsa/queue/CircularQueue.java
@@ -9,20 +9,36 @@ public class CircularQueue<E> implements Que<E> {
     private int front;
     private int rear;
 
+    /**
+     * To construct a array of capacity of size + 1 and to innitailse the front and rear to zero
+     * @param size the size of the array needed.
+     */
     public CircularQueue(int size) {
         capacity = size + 1;
         array = new Object[capacity];
         front = rear = 0;
     }
 
+    /**
+     * To check whether the queue is empty or not.
+     * @return 
+     */
     boolean isEmpty() {
         return front == rear;
     }
 
+    /**
+     * To check whterht the queue is full or not. 
+     * @return
+     */
     boolean isFull() {
         return front == (rear + 1) % capacity;
     }
 
+     
+    /**
+     * @param value the value to be enqueued.
+     */
     public void enqueue(E value) throws QueueFullException {
         if (!isFull()) {
             array[rear] = value;
@@ -31,6 +47,9 @@ public class CircularQueue<E> implements Que<E> {
             throw new QueueFullException("Queue over flow, enqueuing failed.");
     }
 
+    /**
+     * @return the value dequeued.
+     */
     @SuppressWarnings("unchecked")
     public E dequeue() throws QueueEmptyException {
         if (!isEmpty()) {
@@ -41,11 +60,17 @@ public class CircularQueue<E> implements Que<E> {
             throw new QueueEmptyException("Queue udner flow, dequeuing failed.");
     }
 
+    /**
+     * @return the value at the front
+     */
     @SuppressWarnings("unchecked")
     public E front() {
         return isEmpty() ? null : (E) array[front];
     }
 
+    /**
+     * @return the value at the rear.
+     */
     @SuppressWarnings("unchecked")
     public E rear() {
         return isEmpty() ? null : (E) array[rear];
