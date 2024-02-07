ace53905dadc3c1a4d46f588507f9e855b12347c
Vasudevshetty <vasudeepu2815@gmail.com>
2024-02-07 20:16:15 +0530

    Added the basic outline of my circualr queue impmenetation looks like. Added all the logics to the operations.

--- dsa/queue/CircularQueue.java
+++ dsa/queue/CircularQueue.java
@@ -1,16 +1,54 @@
 package dsa.queue;
 
-public class CircularQueue<E> implements Que<E>{
-    public void enqueue(E value){
+import dsa.queue.Exception.QueueEmptyException;
+import dsa.queue.Exception.QueueFullException;
 
+public class CircularQueue<E> implements Que<E> {
+    private Object[] array;
+    private int capacity;
+    private int front;
+    private int rear;
+
+    public CircularQueue(int size) {
+        capacity = size + 1;
+        array = new Object[capacity];
+        front = rear = 0;
     }
-    public E dequeue(){
-        return null;
+
+    boolean isEmpty() {
+        return front == rear;
     }
-    public int length(){
-        return 0;
+
+    boolean isFull() {
+        return front == (rear + 1) % capacity;
     }
-    public E peek(){
-        return null;
+
+    public void enqueue(E value) throws QueueFullException{
+        if (!isFull()) {
+            array[rear] = value;
+            rear = (rear + 1) % capacity;
+        } else
+            throw new QueueFullException("Queue over flow, enqueuing failed.");
     }
+
+    @SuppressWarnings("unchecked")
+    public E dequeue() throws QueueEmptyException{
+        if (!isEmpty()) {
+            E data = (E)array[front];
+            front = (front + 1) % capacity;
+            return data;
+        } else
+            throw new QueueEmptyException("Queue udner flow, dequeuing failed.");
+    }
+
+    @SuppressWarnings("unchecked")
+    public E front() {
+        return isEmpty() ? null : (E) array[front];
+    }
+
+    @SuppressWarnings("unchecked")
+    public E rear() {
+        return isEmpty() ? null : (E) array[rear];
+    }
+
 }
