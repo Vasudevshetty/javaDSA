a977fef9fc916dcfa855073d234714a05d280938
Vasudevshetty <vasudeepu2815@gmail.com>
2024-02-07 19:36:43 +0530

    Added inital level of code with all the functions to it using double linked list.

--- dsa/queue/Deque.java
+++ dsa/queue/Deque.java
@@ -1,5 +1,43 @@
 package dsa.queue;
 
-public class Deque {
-    
+import dsa.list.DoubleLinkedList;
+import dsa.queue.Exception.QueueEmptyException;
+
+public class Deque<E> {
+    DoubleLinkedList<E> que;
+
+    public Deque() {
+        que = new DoubleLinkedList<>();
+    }
+
+    public boolean isEmpty() {
+        return que.isEmpty();
+    }
+
+    public void enqueueFront(E data) {
+        que.insertHead(data);
+    }
+
+    public void euqueueRear(E data) {
+        que.insertTail(data);
+    }
+
+    public E dequeueFront() throws QueueEmptyException {
+        if (isEmpty())
+            throw new QueueEmptyException("Queue under flow, dequeuing failed.");
+        return que.deleteHead();
+    }
+
+    public E dequeueRear() throws QueueEmptyException{
+        if(isEmpty()) throw new QueueEmptyException("Queue under flow, dequeuin failed.");
+        return que.deleteTail();
+    }
+
+    public E front() {
+        return isEmpty() ? null : que.getHead().getData();
+    }
+
+    public E rear() {
+        return isEmpty() ? null : que.getTail().getData();
+    }
 }
