026795783f7a3003220bc39bfd624d95cacf22df
Vasudevshetty <vasudeepu2815@gmail.com>
2024-02-25 16:30:10 +0530

    Completed one step of Linkedlist gui. Shoudl add DRY, comment out and test many cases.

--- gui/ListMenu.java
+++ gui/ListMenu.java
@@ -9,15 +9,21 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+
+import dsa.list.DoubleCircularLinkedList;
+import dsa.list.Exception.EmptyLinkedListException;
 
 public class ListMenu extends JFrame {
     private JTextField dataField;
     private JLabel instructionLabel;
-    private JLabel outputLabel;
+    private JTextArea outputArea;
     private JButton[] buttons;
+    private DoubleCircularLinkedList<String> list;
 
     public ListMenu() {
         super("Linked List Operations");
@@ -49,10 +55,11 @@ public class ListMenu extends JFrame {
     }
 
     private void initaliseComponents() {
-        dataField = new JTextField(15);
+        dataField = new JTextField("", 15);
         instructionLabel = new JLabel("Enter the operation: ");
-        outputLabel = new JLabel("Your output will be displayed here.");
+        outputArea = new JTextArea("Your output will be displayed here.");
         buttons = new JButton[11];
+        list = new DoubleCircularLinkedList<>();
 
         buttons[0] = new JButton("Insert At Head");
         buttons[1] = new JButton("Insert At Tail");
@@ -82,11 +89,16 @@ public class ListMenu extends JFrame {
         constraints.insets = new Insets(0, 10, 10, 10);
         inputOutputPanel.add(dataField, constraints);
 
+        outputArea.setEditable(false);
+        outputArea.setWrapStyleWord(true);
+        outputArea.setLineWrap(true);
         constraints.gridx = 1;
         constraints.gridy = 2;
-        constraints.gridwidth = 2;
+        constraints.gridwidth = 4;
         constraints.fill = GridBagConstraints.BOTH;
-        inputOutputPanel.add(outputLabel, constraints);
+        constraints.weighty = 1.0;
+        constraints.weightx = 1.0;
+        inputOutputPanel.add(outputArea, constraints);
         return inputOutputPanel;
     }
 
@@ -111,7 +123,141 @@ public class ListMenu extends JFrame {
     private class ButtonListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
+            String source = ((JButton) e.getSource()).getText();
+            handleMenu(source);
+        }
+    }
 
+    private void handleMenu(String source) {
+        String data;
+        int position;
+
+        switch (source) {
+            case "Insert At Head":
+                data = dataField.getText().trim();
+                if (!data.isEmpty()) {
+                    instructionLabel.setText("Enter data : ");
+                    list.insertHead(data);
+                    handleMenu("Display");
+                } else {
+                    JOptionPane.showMessageDialog(this, "Please enter valid data.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+                break;
+            case "Insert At Tail":
+                data = dataField.getText().trim();
+                if (!data.isEmpty()) {
+                    instructionLabel.setText("Enter data : ");
+                    list.insertTail(data);
+                    this.handleMenu("Display");
+                } else {
+                    JOptionPane.showMessageDialog(this, "Please enter valid data.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+                break;
+            case "Insert At Specific Position":
+                data = dataField.getText().trim();
+                if (!data.isEmpty()) {
+                    instructionLabel.setText("Enter position : ");
+                    position = Integer.parseInt(data);
+                    instructionLabel.setText("Enter data : ");
+                    data = dataField.getText().trim();
+                    if (!data.isEmpty()) {
+                        list.insert(data, position);
+                        this.handleMenu("Display");
+                    } else {
+                        JOptionPane.showMessageDialog(this, "Please enter valid data.", "Error",
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(this, "Please enter valid position.", "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+                break;
+            case "Delete At Head":
+                try {
+
+                    list.deleteHead();
+                } catch (EmptyLinkedListException e) {
+                    JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    break;
+                }
+                this.handleMenu("Display");
+                break;
+            case "Delete At Tail":
+                try {
+
+                    list.deleteTail();
+                } catch (EmptyLinkedListException e) {
+                    JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    break;
+                }
+                this.handleMenu("Display");
+                break;
+            case "Delete At Specific Position":
+                data = dataField.getText().trim();
+                if (!data.isEmpty()) {
+                    position = Integer.parseInt(data);
+                    try {
+                        list.delete(position);
+                    } catch (EmptyLinkedListException e) {
+                        JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                        break;
+                    }
+                    this.handleMenu("Display");
+                } else {
+                    JOptionPane.showMessageDialog(this, "Please enter valid position.", "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+                break;
+            case "Delete key":
+                data = dataField.getText().trim();
+                if (!data.isEmpty()) {
+                    try {
+                        list.deleteBykey(data);
+                    } catch (EmptyLinkedListException e) {
+                        JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                        break;
+                    }
+                    this.handleMenu("Display");
+                } else {
+                    JOptionPane.showMessageDialog(this, "Please enter valid key.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+                break;
+            case "Search Key":
+                data = dataField.getText().trim();
+                if (!data.isEmpty()) {
+                    list.searchByKey(data);
+                } else {
+                    JOptionPane.showMessageDialog(this, "Please enter valid key.", "Error", JOptionPane.ERROR_MESSAGE);
+                    break;
+                }
+                break;
+            case "Reverse":
+                try {
+                    list.reverse();
+                } catch (EmptyLinkedListException e) {
+                    JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    break;
+                }
+                this.handleMenu("Display");
+                break;
+            case "Copy":
+                @SuppressWarnings("Unchecked")
+                DoubleCircularLinkedList<String> copy = new DoubleCircularLinkedList<>();
+                try {
+
+                    copy = (DoubleCircularLinkedList<String>) list.copyList();
+                } catch (EmptyLinkedListException e) {
+                    JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                }
+                this.handleMenu(copy.toString());
+                break;
+            case "Display":
+                try {
+                    outputArea.setText(list.toString());
+                } catch (EmptyLinkedListException e) {
+                    JOptionPane.showMessageDialog(this, "Empty list cannot be displayed");
+                }
+                break;
         }
     }
 
