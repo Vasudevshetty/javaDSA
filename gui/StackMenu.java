dd100ecd935ada8ff5309c42b0ea8f1664bafda4
Vasudevshetty <vasudeepu2815@gmail.com>
2024-02-25 19:32:00 +0530

    Added layout to the stack menu, logic is yet to be added. Refactor and other things are pending.

--- gui/StackMenu.java
+++ gui/StackMenu.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -9,6 +10,7 @@ import javax.swing.SwingUtilities;
 import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
 import java.awt.GridBagLayout;
 
 import dsa.stack.StackLL;
@@ -22,18 +24,13 @@ public class StackMenu extends JFrame {
 
     private StackLL<String> stack;
 
+    String data;
+
     public StackMenu() {
         super("Stack Menu");
 
         initComponents();
-
-        this.setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
-
-        JPanel buttons = new JPanel(new GridLayout(0, 3, 10, 10));
-        buttons.add(push);
-        buttons.add(pop);
-        buttons.add(peek);
+        setupLayout();
 
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -47,7 +44,44 @@ public class StackMenu extends JFrame {
         peek = new JButton("Peek");
         stack = new StackLL<>();
         outputTextArea = new JTextArea("The output will be visible here.");
+        outputTextArea.setEditable(false);
+        outputTextArea.setWrapStyleWord(true);
+        outputTextArea.setLineWrap(true);
         inputTextField = new JTextField(15);
+    }
+
+    private void setupLayout() {
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridheight = 2;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.insets = new Insets(10, 10, 10, 10);
+        this.add(inputTextField, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridheight = 2;
+        constraints.gridwidth = 2;
+        this.add(outputTextArea, constraints);
+
+        JPanel buttons = new JPanel(new GridLayout(0, 3, 10, 10));
+        buttons.add(push);
+        buttons.add(pop);
+        buttons.add(peek);
+
+        constraints.gridx = 0;
+        constraints.gridy = 4;
+        constraints.gridheight = 2;
+        constraints.gridwidth = 2;
+        this.add(buttons, constraints);
+    }
+
+    private void updateOutput() {
+        outputTextArea.setText(stack.toString());
     }
 
     public static void main(String[] args) {
