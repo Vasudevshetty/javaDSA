4f6ff9ab9c0a830b6c226aeb858e16fa3abc345e
Vasudevshetty <vasudeepu2815@gmail.com>
2024-02-25 12:45:39 +0530

    Reorganised my layout to another idea, the idea's basic layout and its intialisation done, the next commit will be on it getting fixed on the frame.

--- gui/ListMenu.java
+++ gui/ListMenu.java
@@ -1,31 +1,49 @@
 package gui;
 
-import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
 
 public class ListMenu extends JFrame {
-    private JTextField text;
+    private JTextField dataField;
+    private JLabel instructionLabel;
+    private JLabel outputLabel;
     private JButton[] buttons;
 
     public ListMenu() {
-        super("List menu");
+        super("Linked List Operations");
 
+        this.initaliseComponents();
+
+        this.setLayout(new GridLayout());
 
         this.setSize(500, 500);
+        this.setMaximumSize(this.getSize());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new ListMenu();
-            }
-        });
+    private void initaliseComponents() {
+        dataField = new JTextField(15);
+        instructionLabel = new JLabel("Enter the operation: ");
+        outputLabel = new JLabel("Your output will be displayed here.");
+        buttons = new JButton[11];
+
+        buttons[0] = new JButton("Insert At Head");
+        buttons[1] = new JButton("Insert At Tail");
+        buttons[2] = new JButton("Insert At Specific Position");
+        buttons[3] = new JButton("Delete At Head");
+        buttons[4] = new JButton("Delete At Tail");
+        buttons[5] = new JButton("Delete At Specific Position");
+        buttons[6] = new JButton("Search Key");
+        buttons[7] = new JButton("Delete Key");
+        buttons[8] = new JButton("Reverse");
+        buttons[9] = new JButton("Copy");
+        buttons[10] = new JButton("Display");
+
     }
+
 }
