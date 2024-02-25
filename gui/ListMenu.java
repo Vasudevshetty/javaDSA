d654bb43072b667c5f1971a5053f055aa2c8b00d
Vasudevshetty <vasudeepu2815@gmail.com>
2024-02-25 13:01:32 +0530

    Complted input output panel's work , with a new functin to crate such panels and its contraints and added back to the main frame. The next commit wil be on the addition of buttons to the mainframe(operations button).

--- gui/ListMenu.java
+++ gui/ListMenu.java
@@ -1,11 +1,16 @@
 package gui;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 public class ListMenu extends JFrame {
     private JTextField dataField;
@@ -19,6 +24,15 @@ public class ListMenu extends JFrame {
         this.initaliseComponents();
 
         this.setLayout(new GridLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        
+        JPanel inputOutputPanel = createInputPanel();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.anchor = GridBagConstraints.NORTH;
+        this.add(inputOutputPanel, constraints);
 
         this.setSize(500, 500);
         this.setMaximumSize(this.getSize());
@@ -43,7 +57,32 @@ public class ListMenu extends JFrame {
         buttons[8] = new JButton("Reverse");
         buttons[9] = new JButton("Copy");
         buttons[10] = new JButton("Display");
-
     }
 
+    private JPanel createInputPanel() {
+        JPanel inputOutputPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 5, 10);
+        inputOutputPanel.add(instructionLabel, constraints);
+
+        constraints.gridy = 1;
+        constraints.insets = new Insets(0, 10, 10, 10);
+        inputOutputPanel.add(dataField, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.BOTH;
+        inputOutputPanel.add(outputLabel, constraints);
+        return inputOutputPanel;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(ListMenu::new);
+    }
 }
