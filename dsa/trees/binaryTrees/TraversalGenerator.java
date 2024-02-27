125468219114815c686112cae8c4be61d54f5d5a
Vasudevshetty <vasudeepu2815@gmail.com>
2024-02-28 00:00:51 +0530

    Added traversal generator utility class which generates the string of the travlersals of the tree. Will later add the fucntions to generate the tree from the given traversals, either wil add methods or create a sepearte utility.

---
+++ dsa/trees/binaryTrees/TraversalGenerator.java
@@ -0,0 +1,19 @@
+package dsa.trees.binaryTrees;
+
+public class TraversalGenerator<E> {
+    public static <E> String preOrder(BinaryTree<E> tree) {
+
+    }
+
+    public static <E> String postOrder(BinaryTree<E> tree) {
+
+    }
+
+    public static <E> String inOrder(BinaryTree<E> tree) {
+
+    }
+
+    public static <E> String levelOrder(BinaryTree<E> tree) {
+
+    }
+}
